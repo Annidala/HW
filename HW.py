d0e6815b4f590d706f09562ffa796e0396e5ac8b
Annie <anniedala@gmail.com>
2017-06-09 14:31:22 +0200

    Now in multiple languages

--- HW.py
+++ HW.py
@@ -1,6 +1,7 @@
 ﻿from __future__ import print_function
-aa = raw_input('Choose your language :')
-Say_hi = { 'Francais':"J'aime Git", 'English':"I <3 Git", 'Deutsch':'Ich liebe Git', 'Italiano':'Ti amo Git}
+print('hello world')
+aa = raw_input("I'm sorry, I don't understand. Choose your language :")
+Say_hi = { 'Francais':"J'aime Git", 'English':"I <3 Git", 'Deutsch':'Ich liebe Git', 'Italiano':'Ti amo Git'}
 Say_tu = { 'Francais':"Git t'aime", 'English':"And Git loves you back", 'Deutsch':'Git liebt dich', 'Italiano':'Git ti ama'}
 print(Say_hi[aa].lower())
 print(Say_tu[aa].lower())
