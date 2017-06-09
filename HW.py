c1d560e809657250fa63de0dccca2f3656af8208
Annie <anniedala@gmail.com>
2017-06-09 15:28:21 +0200

    less crappy commit

--- HW.py
+++ HW.py
@@ -1,7 +1,7 @@
 ﻿#coding : utf-8
 from __future__ import print_function
 #comment your code !!!
-print('hello world')
+print('hello world, <3')
 aa = raw_input("I'm sorry, I don't understand. Choose your language :")
 Say_hi = { 'Francais':"J'aime Git", 'English':"I <3 Git", 'Deutsch':'Ich liebe Git', 'Italiano':'Ti amo Git'}
 Say_tu = { 'Francais':"Git t'aime aussi !", 'English':"And Git loves you back", 'Deutsch':'Git liebt dich auch', 'Italiano':'Git ti ama'}
