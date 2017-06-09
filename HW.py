f71e3b3c70985ee47eff32dfbdaa94552ccebb84
Annie <anniedala@gmail.com>
2017-06-09 15:07:25 +0200

    add an italian version of HW

--- HW.py
+++ HW.py
@@ -1,6 +1,6 @@
 ﻿from __future__ import print_function
 #comment your code !!!
-print('hello world')
+print('Buongiorno !')
 aa = raw_input("I'm sorry, I don't understand. Choose your language :")
 Say_hi = { 'Francais':"J'aime Git", 'English':"I <3 Git", 'Deutsch':'Ich liebe Git', 'Italiano':'Ti amo Git'}
 Say_tu = { 'Francais':"Git t'aime aussi !", 'English':"And Git loves you back", 'Deutsch':'Git liebt dich auch', 'Italiano':'Git ti ama'}
